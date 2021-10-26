c6fb3a015627c31ef303f7547ae3ec76328f55bb
disku0001 <54368079+disku0001@users.noreply.github.com>
2021-10-26 21:33:25 +0700

    Create 468x60.js

---
+++ js/468x60.js
@@ -0,0 +1,4 @@
+<!--[ 468x60 Ad Tag ( 6x8.website ) - mfk-cpm // www.mfk-cpm.com // ]-->
+<script type="text/javascript"> var a = "1584"; var b = "468x60"; var network = "mfk-cpm.com"; </script>
+<script type="text/javascript" src="https://www.mfk-cpm.com/serve/ads.js"></script>
+<!--[ End of the Ad Tag - mfk-cpm // www.mfk-cpm.com // ]-->
